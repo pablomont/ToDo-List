4723b10f4a533d89795e75c15bdbc37cbe9e1b5c
Pablo Monteiro <paablomt@gmail.com>
2018-03-26 22:17:51 -0300

    utilização da $inject no itemModel

--- app/assets/javascripts/angular/models/item.js
+++ app/assets/javascripts/angular/models/item.js
@@ -1,8 +1,11 @@
 var app = angular.module('app');
 
-app.factory("itemModel",function($resource){
+app.factory('itemModel',itemModel);
+itemModel.$inject = ['$resource']
+
+function  itemModel($resource){
     return $resource("items/:id",{ id: '@id'},{
         query: { method: 'GET', isArray: true},
         update:  { method: 'PUT', responseType: 'json' }
     })
-})
+}
